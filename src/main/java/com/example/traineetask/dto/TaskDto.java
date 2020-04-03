0c4f86705a5bec5e25105dc57d05d8288915f987
MarDatsko <datskomar@gmail.com>
2020-04-04 00:16:20 +0300

    added some Dto classes

---
+++ src/main/java/com/example/traineetask/dto/TaskDto.java
@@ -0,0 +1,9 @@
+package com.example.traineetask.dto;
+
+import lombok.Data;
+
+@Data
+public class TaskDto {
+    String title;
+    String description;
+}
---
+++ src/main/java/com/example/traineetask/dto/UserDto.java
@@ -0,0 +1,10 @@
+package com.example.traineetask.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+    private String firstName;
+    private String lastName;
+    private String email;
+}
